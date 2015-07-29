d892934d42bbb92b8dbd948f180d93f34c521efa
Alex Panov <alpaix@outlook.com>
2015-07-28 19:26:19 -0700

    Initial version of TriggerHostConfiguration w/HostTest

---
+++ HostTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace HostTest
+{
+    class Program
+    {
+        static void Main(string[] vargStrings)
+        {
+            var configuration = new JobHostConfiguration();
+            configuration.Queues.MaxPollingInterval = TimeSpan.FromSeconds(30);
+            configuration.Queues.MaxDequeueCount = 10;
+            configuration.Queues.BatchSize = 1;
+
+            var host = new JobHost(configuration);
+            host.Start();
+
+            // Stop the host if Ctrl + C/Ctrl + Break is pressed
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                host.Stop();
+            };
+
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ SampleWebJob1/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace SampleWebJob1
+{
+    public class Order
+    {
+        public string Name { get; set; }
+
+        public string OrderId { get; set; }
+    }
+
+    public class Functions
+    {
+        public static void QueueTriggerMethod([QueueTrigger("orders")] Order order, int dequeueCount, TextWriter log)
+        {
+            log.WriteLine("New order from: {0}", order.Name);
+            log.WriteLine("Message dequeued {0} times", dequeueCount);
+        }
+    }
+}
